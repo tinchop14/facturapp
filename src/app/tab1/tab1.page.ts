bf510ee38ea1db19bb54f4d4b98cb7d38768a121
martin <tinchop.14@gmail.com>
2020-01-15 15:29:17 -0300

    boton e icono

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,25 +10,6 @@ export class Tab1Page {
   listItems: any;
 
   constructor() {
-    this.listItems = [
-      "1. Aylin Roberts",
-      "2. Autumn Kuhic",
-      "3. Tiffany Windler",
-      "4. Sheila Bauch",
-      "5. Diana Gerhold",
-      "6. Arielle Kuhn"
-    ];
-  }
 
-  onRenderItems(event) {
-    console.log(`Moving item from ${event.detail.from} to ${event.detail.to}`);
-     let draggedItem = this.listItems.splice(event.detail.from,1)[0];
-     this.listItems.splice(event.detail.to,0,draggedItem)
-    //this.listItems = reorderArray(this.listItems, event.detail.from, event.detail.to);
-    event.detail.complete();
-  }
-
-  getList() {
-    console.table(this.listItems);
   }
 }
